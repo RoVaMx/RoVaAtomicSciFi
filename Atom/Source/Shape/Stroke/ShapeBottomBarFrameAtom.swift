aecae5ff7111b81f8a40dc18dc1eb08365b01249
My Name <Francisco.romero.valle.01@gmail.com>
2025-01-16 18:41:29 -0600

    Create ShapeBottomBarFrameAtom.swift

---
+++ Atom/Source/Shape/Stroke/ShapeBottomBarFrameAtom.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Francisco Romero on 16/01/25.
+//
+
+import SwiftUI
+import RoVaAtomicSciFiCore
+
+struct ShapeBottomBarFrameAtom : AtomShape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let p1 = CGPoint(x: rect.minX, y: rect.minY)
+        let p2 = CGPoint(x: rect.maxX, y: rect.minY)
+        
+        path.move(to: p1)
+        path.addLine(to: p2)
+        
+        return path
+    }
+}
