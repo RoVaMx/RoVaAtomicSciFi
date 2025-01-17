0ee0e09c81f9932b2bb77eed1dfd1252212e2859
My Name <Francisco.romero.valle.01@gmail.com>
2025-01-16 18:41:32 -0600

    Create ShapeCrossGridAtom.swift

---
+++ Atom/Source/Shape/Stroke/ShapeCrossGridAtom.swift
@@ -0,0 +1,34 @@
+//
+//  ShapeCrossGridAtom.swift
+//
+//
+//  Created by Francisco Romero on 16/01/25.
+//
+
+import SwiftUI
+import RoVaAtomicSciFiCore
+
+struct ShapeCrossGridAtom : AtomShape {
+    
+    var rows : Int
+    var columns : Int
+    var size: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let w = rect.maxX / CGFloat(columns)
+        let h = rect.maxY / CGFloat(rows)
+        
+        for c in 0...columns {
+            for r in 0...rows {
+                path.addCross(
+                    at: CGPoint(
+                        x: CGFloat(c) * w,
+                        y: CGFloat(r) * h),
+                    size: 5
+                )
+            }
+        }
+        return path
+    }
+}
